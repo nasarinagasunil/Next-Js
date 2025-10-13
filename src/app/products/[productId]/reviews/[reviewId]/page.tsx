adebf2b7c8a0638e5cccd07f25a58c6b4907bd88
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-14 00:22:03 +0530

    page not found

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 export default async function ReviewDetails({params}: {params: Promise<{productId: string, reviewId: string}>}) {
     const {productId, reviewId} = await params;
+    if(parseInt(reviewId) > 1000){
+        notFound();
+    }
     return (
-        <h1>Review Id {reviewId}  for product {productId}</h1>
+        <h1>Review {reviewId}  for product {productId}</h1>
     )
 }
