368801277c2034909a5542961b3fe7e08a027432
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-14 20:04:28 +0530

    linking component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link"
 export default function Home() {
     return (
-        <h1>Welcome to the Home page</h1>
+        <>
+            <h1>Welcome to the Home page</h1>
+            <Link href="/blog">Blog</Link>
+            <Link href="/products">Products</Link>
+        </>
+        
     )
 }
