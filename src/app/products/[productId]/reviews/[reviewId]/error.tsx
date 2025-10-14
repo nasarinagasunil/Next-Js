1e70769700a18cea502f1b19cbdd9b4c93ba6ef9
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-15 00:49:31 +0530

    error handling

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function ErrorBoundary({error}: {error: Error}) {
+    return (
+        <p>{error.message}</p>
+    )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -5,7 +5,15 @@ export default async function ReviewDetails({params}: {params: Promise<{productI
         // notFound();
         redirect("/products")
     }
+    const randomNumber = Math.floor(Math.random()*2);
+    if (randomNumber === 1) {
+        throw new Error("Random Error occured in Review");
+    }
     return (
-        <h1>Review {reviewId}  for product {productId}</h1>
+        <>
+            
+            <h1>Review {reviewId}  for product {productId}</h1>
+        </>
+        
     )
 }
