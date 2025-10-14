9c8ca4e0aa02d916eefd3bacc9f9db0327938b8a
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-14 18:12:39 +0530

    routing metadata using generateMetadata function

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,16 @@
-export default async function ProductDetails({params}: {params: Promise<{productId: string}>}) {
+import { Metadata } from "next";
+
+type props = {params: Promise<{productId: string}>}
+
+export const generateMetadata = async ({params}: props): Promise<Metadata> => {
+    const {productId} = await params;
+    const title = await new Promise((resolve) => setTimeout(() => resolve(`product ${productId}`),1000));
+    return {
+        title: `Product ${title}`,
+    }
+}
+
+export default async function ProductDetails({params}: props) {
     const {productId} =await params;
      return (
         <h1>
