c19ca85f627c917fed4468def20d782c7910d7d2
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-14 23:41:38 +0530

    navigating programatically

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,9 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 export default async function ReviewDetails({params}: {params: Promise<{productId: string, reviewId: string}>}) {
     const {productId, reviewId} = await params;
     if(parseInt(reviewId) > 1000){
-        notFound();
+        // notFound();
+        redirect("/products")
     }
     return (
         <h1>Review {reviewId}  for product {productId}</h1>
