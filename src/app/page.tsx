e899b2976c4d6be12ff655e214c4512be415ec64
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-14 23:21:34 +0530

    params and searchparams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
             <h1>Welcome to the Home page</h1>
             <Link href="/blog">Blog</Link>
             <Link href="/products">Products</Link>
+            <Link href="/articles/breaking-news-123?lang=en">News in English</Link>
+            <Link href="/articles/breaking-news-123?lang=fr">News in French</Link>
         </>
         
     )
