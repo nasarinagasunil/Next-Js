ea2c03e10608f9f0ef78f1c824301857df270e1e
nasarinagasunil <nagasunilnasari@gmail.com>
2025-10-15 15:37:40 +0530

    inntercepting routes: (..) to match segments one level above

--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -6,6 +6,8 @@ export default function F1Page() {
         <h1>F1 Page</h1>
         <div>
             <Link href="/f1/f2">F2</Link>
+            <Link href="/f3">F3</Link>
+
         </div>
         </>
     )
